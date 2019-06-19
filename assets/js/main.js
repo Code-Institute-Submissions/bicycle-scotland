481fd7b004fcf0c8814fdaba24f67f8fd82cd2df
Ubuntu <ubuntu@ip-172-31-80-206.ec2.internal>
2019-06-19 11:10:56 +0000

    adding assets

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $(".north-1").click(function() {
+        $(".north").slideToggle();
+    });
+
+    $(".east-1").click(function() {
+        $(".east").slideToggle();
+    });
+
+    $(".south-1").click(function() {
+        $(".south").slideToggle();
+    });
+
+    $(".west-1").click(function() {
+        $(".west").slideToggle();
+    });
+
+    $(".central-1").click(function() {
+        $(".central").slideToggle();
+    });
+});
+
+$(".option").on("click", function() {
+    $(".opener").hide("slow");
+});
+
+function() {
+    if ($(".option") == (************)) {
+        $(".opener").show("slow");
+    }
+});
