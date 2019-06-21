90f16b2b5500448e7eabf7f77d72c817a4e52d10
Ubuntu <ubuntu@ip-172-31-80-206.ec2.internal>
2019-06-21 13:15:58 +0000

    google maps added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,29 @@
 $(document).ready(function() {
     $(".north-1").click(function() {
-        $(".north").slideToggle();
+        $(".north").slideToggle("slow");
     });
 
     $(".east-1").click(function() {
-        $(".east").slideToggle();
+        $(".east").slideToggle("slow");
     });
 
     $(".south-1").click(function() {
-        $(".south").slideToggle();
+        $(".south").slideToggle("slow");
     });
 
     $(".west-1").click(function() {
-        $(".west").slideToggle();
+        $(".west").slideToggle("slow");
     });
 
     $(".central-1").click(function() {
-        $(".central").slideToggle();
+        $(".central").slideToggle("slow");
+    });
+
+    $(".option").click(function() {
+        $(this).toggleClass("red");
     });
 });
 
 $(".option").on("click", function() {
     $(".opener").hide("slow");
-});
-
-function() {
-    if ($(".option") == (************)) {
-        $(".opener").show("slow");
-    }
 });
