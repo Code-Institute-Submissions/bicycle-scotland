85f35ff603442db903020fc2cb42e50ade9443ad
Ubuntu <ubuntu@ip-172-31-80-206.ec2.internal>
2019-06-21 16:59:16 +0000

    moving all js files into main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,4 +28,33 @@ $(".option").on("click", function() {
     $(".opener").hide("slow");
 });
 
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: {
+            lat: 56.4907,
+            lng: -4.2026
+        }
+    });
 
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    var locations = [
+        { lat: 56.3745, lng: -3.3546 },
+        { lat: 56.2543, lng: -3.2056 },
+        { lat: 56.3410, lng: -3.8710 },
+        { lat: 56.9458, lng: -2.1972 },
+        { lat: 57.5759, lng: -5.7430 },
+        { lat: 56.4157, lng: -5.4692 },
+        { lat: 55.6048, lng: -2.4599 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+};
